a8ecc57767ad4eebfc512801f0574aac46575da9
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-01-19 13:39:31 +0900

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { createElement as h } from "https://esm.sh/react";
+import React from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom";
 
 const data = {
@@ -7,72 +7,42 @@ const data = {
     "리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.",
 };
 
-const anotherData = {
-  greetingMessage: ["행복한 아침이야", "오늘도 좋은 하루 되렴~"],
-  message: "나날이 성장하는 여러분을 보는 즐거움이 쏠쏠~ 😉",
-};
-
-const getGreetMessage = (condition = true) => {
-  let greet = "";
-
-  if (condition) {
-    greet = data.greetingMessage[0].toUpperCase();
-  } else {
-    greet = data.greetingMessage[0].toLowerCase();
-  }
-
-  return greet;
-};
-
-const createApp = (data) => {
+const createApp = (
+  { greetingMessage: [firstMessage, lastMessage], message },
+  options = {}
+) => {
   return (
     <div id="app">
       <h1>
-        {getGreetMessage()}
-        {/* <br /> */}
-        {data.greetingMessage[1].toUpperCase()}
+        {firstMessage.toUpperCase()}
+        <br />
+        {lastMessage.toUpperCase()}
       </h1>
-      <p>{data.message}</p>
+      <p>{message}</p>
+      <form>
+        <input
+          type="range"
+          min={options.min}
+          max={options.max}
+          step={options.step}
+          aria-label={options.label}
+          disabled={options.isDisabled}
+        />
+        <button type="submit">보내기</button>
+      </form>
     </div>
-  );
-};
-
-const createApp2 = (data) => {
-  return h(
-    "div",
-    { id: "app" },
-    h(
-      "h1",
-      {},
-      data.greetingMessage[0].toUpperCase(),
-      // h('br'),
-      data.greetingMessage[1].toUpperCase()
-    ),
-    h("p", null, data.message)
   );
 };
 
 const rootElement = document.getElementById("root");
 const reactDomRoot = createRoot(rootElement);
 
-function render(mode = "data") {
-  reactDomRoot.render(createApp2(mode === "data" ? data : anotherData));
-}
-
-render();
-
-const button = document.querySelector("button");
-
-let mode = "data"; // 'data' | 'anotherData'
-
-const handleChangeMessage = () => {
-  if (mode.includes("data")) {
-    mode = "anotherData";
-  } else {
-    mode = "data";
-  }
-
-  render(mode);
-};
-
-button.addEventListener("click", handleChangeMessage);
+reactDomRoot.render(
+  createApp(data, {
+    label: "중요도",
+    isDisabled: false,
+    min: 10,
+    step: 2,
+    max: 20,
+  })
+);
