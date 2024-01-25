70fe94024b242ffa2cf8c144416e5a9ac00b227b
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-01-25 09:22:22 +0900

    aa

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,7 @@
+import './Avatar.css';
+
+function Avatar /* props */({ photo, isOnline = false }) {
+  return null;
+}
+
+export default Avatar;
---
+++ src/lecture/26-render-list-avatar-selection.jsx
@@ -0,0 +1,12 @@
+import Avatar from '../components/Avatar/Avatar';
+
+function Exercise() {
+  return (
+    <>
+      <Avatar isOnline={true}></Avatar>
+      <Avatar isOnline={false}></Avatar>
+    </>
+  );
+}
+
+export default Exercise;
