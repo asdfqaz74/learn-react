546a77e6997a9cdbc187dcd6fce7e04c5bfaf791
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-01-30 11:22:07 +0900

    aa

---
+++ src/lecture/34-object-array-state-manage.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import catsData from '../data/cats.json';
+import { getStaticImage } from '../utils';
+
+function CatsList() {
+  // 어떤 상태?
+  // 고양이 집합(catsData) : Array
+  const [cats, setCats] = useState(catsData);
+
+  const handleDeleteCat = () => {
+    // [1] 새로운 값 설정
+    setCats(cats.filter((cat) => !cat.birthday.includes('1974')));
+
+    // [2] 이전 값을 연산해서 반환한 값 설정
+    // setCats((cats) => cats.filter((cat) => !cat.birthday.includes('1974')));
+  };
+
+  return (
+    <ul style={{ listStyle: 'none', paddingInlineStart: 0, marginBlock: 0 }}>
+      {cats.map((cat) => (
+        <li key={cat.id}>
+          <img
+            height={100}
+            src={getStaticImage(cat.imageSrc)}
+            alt={cat.imageAlt}
+          />
+          <button
+            type="button"
+            aria-label="삭제"
+            title="삭제"
+            onClick={handleDeleteCat}
+          >
+            ×
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Exercise() {
+  return (
+    <div>
+      <h2>객체형 상태 관리</h2>
+      <CatsList></CatsList>
+    </div>
+  );
+}
