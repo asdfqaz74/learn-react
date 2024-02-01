0193c22b594d8c027e02d9c8cba907201c3f6b53
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-02-01 21:41:45 +0900

    aa

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -10,30 +10,28 @@ export default function Stack({
   mx: marginInline,
   ...restProps
 }) {
-  
   const 컴포넌트_클래스이름 = `${classes.Stack} ${customClassNames}`.trim();
 
-  const 컴폰넌트_제작자의_스타일 = {};
+  const 컴포넌트_제작자의_스타일 = {};
 
   if (vertical) {
-    컴폰넌트_제작자의_스타일['--direction'] = 'column';
+    컴포넌트_제작자의_스타일['--direction'] = 'column';
   }
 
   if (gap || gap === 0) {
-    컴폰넌트_제작자의_스타일['--gap'] = `${gap}px`;
+    컴포넌트_제작자의_스타일['--gap'] = `${gap}px`;
   }
 
   if (marginBlock || marginBlock === 0) {
-    컴폰넌트_제작자의_스타일['--marginBlock'] = `${marginBlock}px`;
+    컴포넌트_제작자의_스타일['--marginBlock'] = `${marginBlock}px`;
   }
 
-  
   if (marginInline || marginInline === 0) {
-    컴폰넌트_제작자의_스타일['--marginInline'] = `${marginInline}px`;
+    컴포넌트_제작자의_스타일['--marginInline'] = `${marginInline}px`;
   }
 
   const 컴포넌트_스타일_옵션 = {
-    ...컴폰넌트_제작자의_스타일,
+    ...컴포넌트_제작자의_스타일,
     ...컴포넌트_사용자의_커스텀스타일,
   };
 
---
+++ src/lecture/38-stack.jsx
@@ -0,0 +1,14 @@
+import { Stack } from '@/components';
+
+export default function Exercise() {
+  return (
+    <>
+      <Stack as="h2">안녕하세요</Stack>
+      <Stack>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </Stack>
+    </>
+  );
+}
